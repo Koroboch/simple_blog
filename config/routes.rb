ad4c8a14d11f1175cf269b904b07b3b0faf6a1df
Koroboch <vkirill1@yandex.ru>
2022-12-01 18:32:13 +0300

    add rout path for Article

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+
   root 'home#index'
 
   get '/terms' => 'pages#terms'
   get '/about' => 'pages#about'
+
+  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
