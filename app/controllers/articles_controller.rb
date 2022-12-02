eb6f4d51bd7b74a95dadd00568797f08dfa7e3cd
Koroboch <vkirill1@yandex.ru>
2022-12-02 14:36:12 +0300

    update edit(add turbo-frames)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,7 +23,7 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     @article.update(article_params)
-    redirect_to @article
+    redirect_to root_path
   end
 
   def destroy
