f74fe03a71c39f3a0aee27464a0fb4051618ed4a
Koroboch <vkirill1@yandex.ru>
2022-12-03 12:17:40 +0300

    add User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_02_140949) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_03_090225) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_02_140949) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
