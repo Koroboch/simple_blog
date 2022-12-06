7fe81d92f84fbee1719cf4f0443f5b6275d6f489
Koroboch <vkirill1@yandex.ru>
2022-12-06 12:43:53 +0300

    add user_id fo Article and Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,8 @@ class CommentsController < ApplicationController
  def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.new(comment_params)
+    @comment.author = current_user.username
+    @comment.user_id = current_user.id
     @comment.save
 
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_03_094558) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_06_091240) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -25,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_03_094558) do
     t.integer "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
